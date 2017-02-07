f7c0ebde8bb96af3b5349c97d9221ab3a6163194
Benjamin Clarke <benjiclarke@me.com>
2017-02-07 21:23:17 +0000

    initial

---
+++ Life/Board.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Life
+{
+	public class Board
+	{
+		private Square[,] _board1;
+		private Square[,] _board2;
+		private Square[,] _currentBoard;
+
+		public Board(int height, int width)
+		{
+			_board1 = new Square[height, width];
+			_board2 = new Square[height, width];
+			-_currentBoard = _board1;
+		}
+	}
+}
+
---
+++ Life/Square.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Life
+{
+
+	public enum State{
+
+		ALIVE,
+		DEAD
+
+	}
+	public class Square
+	{
+		private State _state;
+		public State State {
+			get{ return _state; }
+			set{ _state = value; }
+		}
+
+		public Square(State s)
+		{
+			State = s;
+		}
+
+		public Square()
+		{
+			State = State.DEAD;
+		}
+
+		public static State randState(){
+			return new Random().Next(2);
+		}
+	}
+}
+
